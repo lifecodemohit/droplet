6d3adca445349317ccdf31b8c5f3810961a06ad2
lifecodemohit <mohit13063@iiitd.ac.in>
2015-03-10 22:30:57 +0530

    Comment updated + quotes handled

--- vendor/csvscript.js
+++ vendor/csvscript.js
@@ -52,24 +52,32 @@ lang   : javascript
 					var each_node_list=[];
 					for(var j=0;j<=entries.length-1;j++)
 					{
+						if(entries[j].match(/^.*\s*\/\/.*$/))
+							entries[j]=entries[j].split('//')[0];
 						//console.log(getLeadingWhiteSpaceCountFor(entries[j]));
 						var leading_space=getLeadingWhiteSpaceCountFor(entries[j]);
 						var trailing_space=getLeadingWhiteSpaceCountFor(entries[j].split("").reverse().join(""));
 						if(entries[j].match(/^\s*\".*$/))
 						{
-							var msg="";
+							var entry_skip="";
 							while((j<entries.length)&&(!entries[j].match(/^.*\s*\"$/)))
 							{
-								msg=msg+entries[j];
-								msg=msg+',';
+								entry_skip=entry_skip+entries[j];
+								entry_skip=entry_skip+',';
 								j++;
 							}
 							if(j<entries.length)
-								msg=msg+entries[j];
-							var each_node_node={type:"each_node_node",start:end_count+count,end:end_count+count+msg.length,loc:{start:{line:i,column:count+leading_space},end:{line:i,column:count+msg.length-trailing_space}},raw:msg};
-							count=count+msg.length+1;
+								entry_skip=entry_skip+entries[j];
+							else
+								return null;
+							var each_node_node={type:"each_node_node",start:end_count+count,end:end_count+count+entry_skip.length,loc:{start:{line:i,column:count+leading_space},end:{line:i,column:count+entry_skip.length-trailing_space}},raw:entry_skip};
+							count=count+entry_skip.length+1;
 							each_node_list.push(each_node_node);
 						}
+						else if(entries[j].match(/^.*\s*\"$/))
+						{
+				    	return null;
+				    }
 						else
 						{
 							var each_node_node={type:"each_node_node",start:end_count+count,end:end_count+count+entries[j].length,loc:{start:{line:i,column:count+leading_space},end:{line:i,column:count+entries[j].length-trailing_space}},raw:entries[j]};
